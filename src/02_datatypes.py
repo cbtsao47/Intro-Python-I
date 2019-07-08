58bb13ff00096a7e191713df14a3281ec2d2d0ac
Ben <cbtsao47@gmail.com>
2019-07-08 15:44:49 -0500

    complete datatypes

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,11 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+y = int(y)
 
+print(str(x + y))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(str(x*10+y))
